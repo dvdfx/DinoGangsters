6d92d478592164c229e91bce55cd62dfb70efafe
rb2610 <rb2610@hotmail.co.uk>
2013-05-25 20:23:07 +0100

    ImgLoad

--- src/Version1/Object.java
+++ src/Version1/Object.java
@@ -4,11 +4,22 @@
  */
 package Version1;
 
+import java.io.*;
+import org.newdawn.slick.opengl.*;
+import org.newdawn.slick.util.ResourceLoader;
+
 /**
  *
  * @author Ramen
  */
 public class Object
 {
+    Texture texture;
+    
     private String imgLoc;
+    
+    public void init() throws IOException
+    {
+        texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(imgLoc));
+    }
 }
