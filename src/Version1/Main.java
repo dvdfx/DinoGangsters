b61d22eae7bb4b93501ca954eb5bd782faa73738
Dave <dave_flameboy@yahoo.co.uk>
2013-05-25 18:32:38 +0100

    Window opens

--- src/Version1/Main.java
+++ src/Version1/Main.java
@@ -13,12 +13,13 @@ public class Main
         Main main = null;
         try
         {
+          main = new Main();
           main.create();
+          main.run();
         }
         catch(Exception ex)
         {
             //LOGGER.log(Level.SEVERE,ex.toString(),ex);
-            //
         }
     }
     
@@ -26,6 +27,25 @@ public class Main
     public void create() throws LWJGLException
     {
       Display.setDisplayMode(new DisplayMode(100,100));
+      Display.setTitle("DinoGangsters");
       Display.create();
+    }
+    
+    public void destroy()
+    {
+        Display.destroy();
+    }
+    
+    public void run()
+    {
+      while(!Display.isCloseRequested())
+      {
+       Display.update();   
+      }
+    }
+    
+    public void update()
+    {
+        
     }
 }
