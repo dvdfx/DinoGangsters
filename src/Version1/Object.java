2deed347d83c724ae33f7c1e3749d2458ac01df8
rb2610 <rb2610@hotmail.co.uk>
2013-05-25 22:39:45 +0100

    Objects and shizzle

--- src/Version1/Object.java
+++ src/Version1/Object.java
@@ -6,6 +6,7 @@ package Version1;
 
 import java.io.*;
 import static org.lwjgl.opengl.GL11.*;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.*;
 import org.newdawn.slick.util.ResourceLoader;
 
@@ -17,14 +18,19 @@ public class Object
 {
     private Texture texture;
     private String imgLoc;
+    private float xPos, yPos, width, height;
     
     public Object()
     {
         ;
     }
     
-    public void init(String iLoc, int w, int h) throws IOException
+    public void init(String iLoc, float x, float y, float w, float h) throws IOException
     {
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
         imgLoc = iLoc;
         System.out.println(imgLoc);
         texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(imgLoc));
@@ -32,15 +38,13 @@ public class Object
     
     public void render()
     {
-        //glTranslatef(0,0,0.0f);
-        glRotatef(0,0.0f,0.0f,1.0f);
-        //glTranslatef(-(100 >> 1),-(100 >> 1),0.0f);
+        Color.white.bind();
         texture.bind();
         glBegin(GL_QUADS);
-            glTexCoord2f(0.0f,0.0f); glVertex2f(60,60);
-            glTexCoord2f(1.0f,0.0f); glVertex2f(960,60);
-            glTexCoord2f(1.0f,1.0f); glVertex2f(960,500);
-            glTexCoord2f(0.0f,1.0f); glVertex2f(60,500);
+            glTexCoord2f(0.0f, 1.0f); glVertex2f(xPos, yPos);
+            glTexCoord2f(1.0f,1.0f); glVertex2f(xPos + width, yPos);
+            glTexCoord2f(1.0f,0.0f); glVertex2f(xPos + width, yPos + height);
+            glTexCoord2f(0.0f,0.0f); glVertex2f(xPos, yPos + height);
         glEnd();
     }
 }
