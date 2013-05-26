c3c397ac360df859d09a8afb841d1d53b75623ea
rb2610 <rb2610@hotmail.co.uk>
2013-05-26 23:24:28 +0100

    AMOOOOOO

---
+++ src/Version1/Loot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Version1;
+
+/**
+ *
+ * @author Ramen
+ */
+public class Loot extends Object
+{
+    public Loot(float x, float y, String t)
+    {
+        super("resource/" + t.toLowerCase() + ".png", x, y, 32, 32, 0, 0, 32, 32);
+        type = t;
+    }
+}
