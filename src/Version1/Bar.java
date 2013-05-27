3e269e50d81447485fde997bc3b1cf2d8abc6b19
rb2610 <rb2610@hotmail.co.uk>
2013-05-27 18:21:00 +0100

    Stuff

---
+++ src/Version1/Bar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Version1;
+
+/**
+ *
+ * @author Ramen
+ */
+public class Bar extends Object
+{
+    public Bar(float x, float y)
+    {
+        super("src/resource/bar.png", x, y, 200, 300, 0, 0, 100, 150);
+        type = "Bar";
+    }
+}
--- src/Version1/Main.java
+++ src/Version1/Main.java
@@ -55,6 +55,7 @@ public class Main
     private Player player;
     private Object menuObj;
     private Object bkgd;
+    private Bar bar;
     private Object GUIObj;
     private Object goverObj;
     private Object mapScreen;
@@ -156,6 +157,7 @@ public class Main
         wObjs.add(player);
         
         bkgd = new Object("src/resource/street5.png", 0.0f, 40.0f, 1780, SCREEN_HEIGHT - 40, 600.0f, 0.0f, 1024.0f, 366.0f);
+        bar = new Bar(1700, 300);
         
         GUIObj = new Object("src/resource/headerBar2.png", 0.0f, 0.0f, SCREEN_WIDTH, 40, 0.0f, 0.0f, 512.0f, 20.0f);
         
@@ -287,6 +289,7 @@ public class Main
                 BeerIcon beerIcon = new BeerIcon(camX + i * 12, SCREEN_HEIGHT - 42);
                 beerIcon.render();
             }
+            bar.render();
         }
     }
     
